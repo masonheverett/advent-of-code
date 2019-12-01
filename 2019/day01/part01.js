5bd1403d4938dd39a385756c85f36770a2070b5b
Quintin Roper <quintinroper@gmail.com>
2019-12-01 16:27:22 -0700

    abstract requirements calculation

--- 2019/day01/part01.js
+++ 2019/day01/part01.js
@@ -2,8 +2,12 @@ const _ = require('lodash')
 
 const solve = (data) => {
   let sum = 0;
-  _.each(data, value => sum += Math.floor((parseInt(value, 10) / 3)) - 2);
+  _.each(data, value => sum += requirements(parseInt(value, 10)));
   console.log(sum);
+}
+
+const requirements = (value) => {
+  return Math.floor(value / 3) - 2
 }
 
 module.exports = { solve }
