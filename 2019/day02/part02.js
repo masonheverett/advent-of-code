f56952f05f093bc2ad282ea5b9c3996eaa5d4fd8
Quintin Roper <quintinroper@gmail.com>
2019-12-02 00:16:06 -0700

    day 2 part 2

--- 2019/day02/part02.js
+++ 2019/day02/part02.js
@@ -1,7 +1,25 @@
 const _ = require('lodash')
 
 const solve = (data) => {
-  console.log('This puzzle has not been attempted')
+  for (let i = 0; i < data.length; i++) {
+    data[i] = parseInt(data[i], 10)
+  }
+  for (let i = 0; i < data.length; i += 4) {
+    switch (data[i]) {
+      case 1:
+        data[data[i + 3]] = data[data[i + 1]] + data[data[i + 2]]
+        break;
+      case 2:
+        data[data[i + 3]] = data[data[i + 1]] * data[data[i + 2]]
+        break;
+      case 99:
+        i = data.length
+        break;
+      default:
+        break;
+    }
+  }
+  console.log(data[0])
 }
 
 module.exports = { solve }
