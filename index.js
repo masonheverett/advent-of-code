be6bfdf9b51b3b64176d0e9a464571cb7ac1cb3c
Mason Everett <masonheverett@gmail.com>
2019-12-13 11:00:56 -0500

    day 13 part 1

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 // ================================================
 
 const YEAR = '2019'
-const DAY = '11'
-const PART = '02'
+const DAY = '13'
+const PART = '01'
 
 // ================================================
 // == Don't touch any of this stuff... please... ==
