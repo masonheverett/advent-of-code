4b01c57c3293a4ea65311cd9154e5791c681caa2
Mason Everett <masonheverett@gmail.com>
2019-12-19 19:45:55 -0500

    Close on part 2

--- 2019/day17/part02.js
+++ 2019/day17/part02.js
@@ -1,7 +1,33 @@
 const _ = require('lodash')
+const os = require('os')
+const runProgram = require('../../intcode.js').runProgram
+
+const main = [65, 44, 66, 44, 65, 44, 66, 44, 67, 44, 65, 44, 67, 44, 65, 44, 67, 44, 66, 10]
+const funA = [82, 44, 56, 44, 52, 44, 76, 44, 56, 44, 76, 44, 52, 44, 76, 44, 52, 10]
+const funB = [76, 44, 56, 44, 82, 44, 54, 44, 76, 44, 54, 10]
+// TODO: funC is too long!!!!!!!!!! See day17/scratch.txt
+const funC = [76, 44, 56, 44, 76, 44, 52, 44, 82, 44, 56, 44, 52, 44, 76, 44, 54, 44, 76, 44, 52, 10]
+const feed = [110, 10]
+const input = main.concat(funA).concat(funB).concat(funC).concat(feed)
 
 const solve = (data) => {
-  console.log('This puzzle has not been attempted')
+  const parsedData = data[0].split(',').map(Number)
+  let state = { data: parsedData }
+  input.forEach(value => {
+    state = runProgram(state, value)
+  })
+  console.log(state.output.map(value => String.fromCharCode(value)).join(''))
+  // console.log(printableGrid(state.output))
+}
+
+const printableGrid = (output) => {
+  const columns = _.indexOf(output, 10)
+  const grid = _.chunk(output, columns + 1).map(row => row.slice(0, -1))
+  return grid.reduce((total, next) => {
+    return total + next.reduce((total1, next1) => {
+      return total1 + String.fromCharCode(next1)
+    }, '') + os.EOL
+  }, '')
 }
 
 module.exports = { solve }
