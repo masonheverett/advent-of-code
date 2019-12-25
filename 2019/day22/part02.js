5f34eb8565de7a5cc597f9875a1f813dfbc9dd15
Quintin Roper <quintinroper@gmail.com>
2019-12-24 16:03:31 -1000

    part 2

--- 2019/day22/part02.js
+++ 2019/day22/part02.js
@@ -1,7 +1,32 @@
 const _ = require('lodash')
+const bigintCryptoUtils = require('bigint-crypto-utils')
 
 const solve = (data) => {
-  console.log('This puzzle has not been attempted')
+  const times = 101741582076661n
+  const deckSize = 119315717514047n
+  const cardPosition = 2020n
+
+  let incMult = 1n
+  let dOffset = 0n
+
+  _.each(data, (inst) => {
+    if (inst === "deal into new stack") {
+      incMult = -incMult % deckSize
+      dOffset = (dOffset + incMult) % deckSize
+    } else if (_.includes(inst, "cut")) {
+      const num = BigInt(inst.match(/-?\d+/))
+      dOffset = (dOffset + num * incMult) % deckSize
+    } else if (_.includes(inst, "deal with increment")) {
+      const num = BigInt(inst.match(/-?\d+/))
+      incMult = (incMult * bigintCryptoUtils.modInv(num, deckSize)) % deckSize
+    }
+  });
+
+  const increment = bigintCryptoUtils.modPow(incMult, times, deckSize)
+
+  let offset = (dOffset * (1n - increment) * bigintCryptoUtils.modInv((1n - incMult) % deckSize, deckSize)) % deckSize
+
+  console.log(Number((offset + increment * cardPosition) % deckSize))
 }
 
 module.exports = { solve }
