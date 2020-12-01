fdc4be6f475f5aab50e72e1b8aeda135b64410fc
Mason Everett <masonheverett@gmail.com>
2020-12-01 06:31:39 -0500

    Finish day 1 2020

--- 2020/day01/part01.js
+++ 2020/day01/part01.js
@@ -1,7 +1,16 @@
 const _ = require('lodash')
 
 const solve = (data) => {
-  console.log('This puzzle has not been attempted')
+  data.some((item1, index1) => {
+    return data.some((item2, index2) => {
+      const num1 = Number(item1)
+      const num2 = Number(item2)
+      if (index1 != index2 && num1 + num2 == 2020) {
+        console.log(num1 * num2)
+        return true
+      }
+    })
+  })
 }
 
 module.exports = { solve }
--- 2020/day01/part02.js
+++ 2020/day01/part02.js
@@ -1,7 +1,19 @@
 const _ = require('lodash')
 
 const solve = (data) => {
-  console.log('This puzzle has not been attempted')
+  data.some((item1, index1) => {
+    return data.some((item2, index2) => {
+      return data.some((item3, index3) => {
+        const num1 = Number(item1)
+        const num2 = Number(item2)
+        const num3 = Number(item3)
+        if (index1 != index2 && index1 != index3 && num1 + num2 + num3 == 2020) {
+          console.log(num1 * num2 * num3)
+          return true
+        }
+      })
+    })
+  })
 }
 
 module.exports = { solve }
