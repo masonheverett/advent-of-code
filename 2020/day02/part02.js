290899557fa5307f12dac7bbb6ccf50da2577532
Quintin Roper <quintin.roper@credera.com>
2020-12-01 20:51:59 -1000

    Day 2 solutions

--- 2020/day02/part02.js
+++ 2020/day02/part02.js
@@ -1,7 +1,20 @@
 const _ = require('lodash')
 
 const solve = (data) => {
-  console.log('This puzzle has not been attempted')
+  var count = 0
+  _.forEach(data, (line) => {
+    const inst = _.split(line, ' ')
+    const locs = _.split(inst[0], '-')
+    const chr = inst[1][0]
+    const x = _.parseInt(locs[0]) - 1
+    const y = _.parseInt(locs[1]) - 1
+    const str = inst[2]
+    if ((str[x] === chr && str[y] !== chr) || (str[x] !== chr && str[y] === chr))
+    {
+      count++
+    }
+  })
+  console.log(count)
 }
 
 module.exports = { solve }
