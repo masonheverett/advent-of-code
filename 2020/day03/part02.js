3fa8afeffb7c58c41b4fea9beee7e55f705cc75a
Quintin Roper <quintin.roper@credera.com>
2020-12-02 21:32:51 -1000

    Day 3 solutions

--- 2020/day03/part02.js
+++ 2020/day03/part02.js
@@ -1,7 +1,30 @@
 const _ = require('lodash')
 
 const solve = (data) => {
-  console.log('This puzzle has not been attempted')
+  var count1 = 0
+  var count3 = 0
+  var count5 = 0
+  var count7 = 0
+  var count2 = 0
+  const width = data[0].length
+  for (i = 1; i < data.length; i++) {
+    const x1 = i % width
+    const x3 = (3 * i) % width
+    const x5 = (5 * i) % width
+    const x7 = (7 * i) % width
+    if (data[i][x1] === '#')
+      count1++;
+    if (data[i][x3] === '#')
+      count3++;
+    if (data[i][x5] === '#')
+      count5++;
+    if (data[i][x7] === '#')
+      count7++;
+    if (i < data.length / 2 && data[2 * i][x1] === '#')
+      count2++;
+  }
+  const out = count1 * count3 * count5 * count7 * count2
+  console.log(count1 + " * " + count3 + " * " + count5 + " * " + count7 + " * " + count2 + " = " + out)
 }
 
 module.exports = { solve }
