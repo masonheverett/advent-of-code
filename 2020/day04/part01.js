3463195fc737236b2b5e6cf671fc68747e69e7ff
Quintin Roper <quintin.roper@credera.com>
2020-12-03 20:55:17 -1000

    Day 4 part 1

--- 2020/day04/part01.js
+++ 2020/day04/part01.js
@@ -1,7 +1,25 @@
 const _ = require('lodash')
 
 const solve = (data) => {
-  console.log('This puzzle has not been attempted')
+  const fields = ['byr:', 'iyr:', 'eyr:', 'hgt:', 'hcl:', 'ecl:', 'pid:']
+  var valid = 0
+  var doc = []
+  for (i = 0; i < data.length; i++) {
+    if (data[i] !== '') {
+      _.forEach(fields, (field) => {
+        if (_.includes(data[i], field)) {
+          doc.push(field)
+        }
+      })
+    }
+    else {
+      if (doc.length === fields.length) {
+        valid++
+      }
+      doc = []
+    }
+  }
+  console.log(valid)
 }
 
 module.exports = { solve }
