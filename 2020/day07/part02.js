4f7ea7a1438aaa8558e85384a628a2ac25398016
Mason Everett <masonheverett@gmail.com>
2020-12-07 11:49:21 -0500

    Day 7 Part 2

--- 2020/day07/part02.js
+++ 2020/day07/part02.js
@@ -1,7 +1,25 @@
 const _ = require('lodash')
 
 const solve = (data) => {
-  console.log('This puzzle has not been attempted')
+  console.log(innerBagCount('shiny gold', parse(data)) - 1)
+}
+
+const innerBagCount = (bagName, rules) => {
+  const bagRule = _.find(rules, (rule) => bagName === rule[0])
+  return 1 + _.sumBy(bagRule[1], (innerBag) => {
+    if (innerBag === 'no other') return 0
+    const count = Number.parseInt(innerBag)
+    const innerBagName = _.tail(innerBag.split(' ')).join(' ')
+    return count * innerBagCount(innerBagName, rules)
+  })
+}
+
+const parse = (data) => {
+  return data.map((rule) => {
+    const slim = rule.replace(/(bags? ?|\.)/g, '').trim()
+    const pair = slim.split(' contain ')
+    return [pair[0], pair[1].split(' , ')]
+  })
 }
 
 module.exports = { solve }
