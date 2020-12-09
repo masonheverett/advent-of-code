7b918885f6606b2d27853927b75946222abad809
Tim Chabot <tim.chabot@credera.com>
2020-12-09 08:43:40 -0600

    Solution for day9 part 1

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 // ================================================
 
 const YEAR = '2020'
-const DAY = '08'
-const PART = '02'
+const DAY = '09'
+const PART = '01'
 
 // ================================================
 // == Don't touch any of this stuff... please... ==
