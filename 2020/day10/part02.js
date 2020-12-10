73101ada57039b0dbd6c0165598c907392fea385
Mason Everett <masonheverett@gmail.com>
2020-12-10 11:37:40 -0500

    Inefficient part 2 solution

--- 2020/day10/part02.js
+++ 2020/day10/part02.js
@@ -1,7 +1,28 @@
 const _ = require('lodash')
 
 const solve = (data) => {
-  console.log('This puzzle has not been attempted')
+  const adapters = parse(data)
+  console.log(arrangements(adapters, 1))
+}
+
+const arrangements = (data, start) => {
+  let count = 1
+  for (let i = start; i < data.length - 1; i++) {
+    if (canRemove(data, i)) {
+      count += arrangements(_.without(data, data[i]), i)
+    }
+  }
+  return count
+}
+
+const canRemove = (data, i) => {
+  return data[i + 1] - data[i - 1] < 4
+}
+
+const parse = (data) => {
+  const sorted = data.map((num) => parseInt(num)).sort((a, b) => a - b)
+  sorted.unshift(0)
+  return sorted
 }
 
 module.exports = { solve }
