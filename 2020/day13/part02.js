af67020e76382c8d0a34094a8e4c1f3cc3923bb5
Mason Everett <masonheverett@gmail.com>
2020-12-14 16:34:04 -0500

    Day 13 part 2

--- 2020/day13/part02.js
+++ 2020/day13/part02.js
@@ -1,7 +1,30 @@
 const _ = require('lodash')
 
 const solve = (data) => {
-  console.log('This puzzle has not been attempted')
+  const buses = parse(data)
+  let num = buses[0][0]
+  let currPost = 1
+  let toAdd = buses[0][0]
+  while (true) {
+    for (let i = currPost; i < buses.length; i++) {
+      if ((num + buses[i][1]) % buses[i][0] === 0) {
+        toAdd *= buses[i][0]
+        currPost++
+      } else {
+        break
+      }
+    }
+    if (currPost === buses.length) break
+    num += toAdd
+  }
+  console.log(num)
+}
+
+const parse = (data) => {
+  return data[1].split(',')
+    .map((id, index) => [id, index])
+    .filter((bus) => bus[0] !== 'x')
+    .map((bus) => [parseInt(bus[0]), bus[1]])
 }
 
 module.exports = { solve }
