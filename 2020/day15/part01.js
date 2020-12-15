2198fc6050d819692a8455fa89bc723744dc5ca8
Mason Everett <masonheverett@gmail.com>
2020-12-15 09:24:06 -0500

    Day 15

--- 2020/day15/part01.js
+++ 2020/day15/part01.js
@@ -1,7 +1,17 @@
 const _ = require('lodash')
 
 const solve = (data) => {
-  console.log('This puzzle has not been attempted')
+  const nums = data[0].split(',').map(Number)
+  while (nums.length < 2020) {
+    nums.push(nextNumber(nums))
+  }
+  console.log(nums[2019])
+}
+
+const nextNumber = (nums) => {
+  const last = _.findLastIndex(nums, (num) => num === nums[nums.length - 1], nums.length - 2)
+  if (last === -1) return 0
+  return nums.length - last - 1
 }
 
 module.exports = { solve }
--- 2020/day15/part02.js
+++ 2020/day15/part02.js
@@ -1,7 +1,28 @@
 const _ = require('lodash')
 
 const solve = (data) => {
-  console.log('This puzzle has not been attempted')
+  const nums = data[0].split(',')
+  const lastSeen = {}
+  nums.forEach((num, index) => {
+    if (index < nums.length - 1) {
+      lastSeen[num] = index
+    }
+  })
+  while (nums.length < 30000000) {
+    const next = nextNumber(nums, lastSeen)
+    lastSeen[nums[nums.length - 1]] = nums.length - 1
+    nums.push(next)
+    if (nums.length % 1000000 === 0) {
+      console.log(`${nums.length / 1000000} million turns in...`)
+    }
+  }
+  console.log(nums[29999999])
+}
+
+const nextNumber = (nums, lastSeen) => {
+  const last = lastSeen[nums[nums.length - 1]]
+  if (last === undefined) return '0'
+  return (nums.length - last - 1).toString()
 }
 
 module.exports = { solve }
