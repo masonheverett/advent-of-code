6358b5a88a33f2fc3208378a15b2a9a09a0d8831
Mason Everett <masonheverett@gmail.com>
2020-12-18 21:25:44 -0500

    Finish day 17

--- 2020/day17/part01.js
+++ 2020/day17/part01.js
@@ -3,104 +3,84 @@ const _ = require('lodash')
 const activeCube = '#'
 
 const solve = (data) => {
-  var cycleMap = new Map()
-  var totalActiveCubes = 0
-
-  _.forEach(data, (row, y) => {
-    _.forEach(row, (cube, x) => {
-      var key = [x,y,0].join(',')
-      cycleMap.set(key, (cube == activeCube))
-    })
+  const source = parse(data)
+  _.times(6, () => {
+    addSpace(source)
+    boot(source)
   })
-
-  for(var cycle=0; cycle<6; cycle++) {
-    var minX = 0
-    var minY = 0
-    var minZ = 0
-    var maxX = 0
-    var maxY = 0
-    var maxZ = 0
-    var cycleKeys = cycleMap.keys()
-    var nextCycleMap = new Map()
-
-    for(cycleKey of cycleKeys) {
-      var aKey = cycleKey.split(',')
-      var x = parseInt(aKey[0])
-      var y = parseInt(aKey[1])
-      var z = parseInt(aKey[2])
-      if(x > maxX) {
-        maxX = x
-      }
-      if(y > maxY) {
-        maxY = y
-      }
-      if(z > maxZ) {
-        maxZ = z
-      }
-      if(x < minX) {
-        minX = x
-      }
-      if(y < minY) {
-        minY = y
-      }
-      if(z < minZ) {
-        minZ = z
-      }
-    }
-   
-    for(var x = minX-1; x <= maxX+1; x++) {
-      for(var y = minY-1; y <= maxY+1; y++) {
-        for(var z = minZ-1; z <= maxZ+1; z++) {
-          var neighbors = getNeighbors(cycleMap, x,y,z)
-          var activeNeighbors = neighbors.filter(val => val).length
-          var key = [x,y,z].join(',')
-          var isActive = false
-          if(cycleMap.has(key)) {
-            isActive = cycleMap.get(key)
-          }
-          if(!isActive && activeNeighbors == 3){
-            nextCycleMap.set(key, true)
-          }
-          else if(isActive && activeNeighbors != 2 && activeNeighbors != 3) {
-            nextCycleMap.set(key, false)
-          }
-          else {
-            nextCycleMap.set(key, isActive)
-          }
-        }
-      }
-    }
-    cycleMap = nextCycleMap
-  }
-
-  var cubes = cycleMap.values()
-  for (var cubeState of cubes) {
-    if(cubeState) {
-      totalActiveCubes++
-    }
-  }
-
-  console.log(totalActiveCubes)
+  console.log(activeCount(source))
 }
 
-const getNeighbors = (map, x,y,z) => {
-  var neighbors = []
-  for(var a = x-1; a <= x+1; a++) {
-    for(var b = y-1; b <= y+1; b++) {
-      for(var c = z-1; c <= z+1; c++) {
-        if(a != x || b != y || c != z) {
-          var key = [a,b,c].join(',')
-          if(map.has(key)) {
-            neighbors.push(map.get(key))
-          }
-          else {
-            neighbors.push(false)
-          }
+const parse = (data) => {
+  return [data.map((row) => row.split(''))]
+}
+
+const addSpace = (source) => {
+  source.forEach((plane) => {
+    plane.forEach((row) => {
+      row.unshift('.')
+      row.push('.')
+    })
+    const rowLength = plane.length + 2
+    plane.unshift(_.times(rowLength, _.constant('.')))
+    plane.push(_.times(rowLength, _.constant('.')))
+  })
+  const planeWidth = source[0][0].length
+  const planeLength = source[0].length
+  source.unshift(_.times(planeLength, () => _.times(planeWidth, _.constant('.'))))
+  source.push(_.times(planeLength, () => _.times(planeWidth, _.constant('.'))))
+}
+
+const boot = (source) => {
+  const oldSource = _.cloneDeep(source)
+  oldSource.forEach((plane, z) => {
+    plane.forEach((row, y) => {
+      row.forEach((cube, x) => {
+        const activeNeighborCount = activeNeighbors(oldSource, x, y, z)
+        if (cube === '.') {
+          source[z][y][x] = activeNeighborCount === 3 ? '#' : '.'
+        } else {
+          source[z][y][x] = [2, 3].includes(activeNeighborCount) ? '#' : '.'
         }
-      }
-    }
-  }
-  return neighbors
+      })
+    })
+  })
+}
+
+const activeNeighbors = (source, x, y, z) => {
+  let activeCount = 0
+  const deltas = [-1, 0, 1]
+  deltas.forEach((dz) => {
+    deltas.forEach((dy) => {
+      deltas.forEach((dx) => {
+        if (dz === 0 && dy === 0 && dx === 0) return
+        const nz = z + dz
+        const ny = y + dy
+        const nx = x + dx
+        if (nz < 0 || ny < 0 || nx < 0) return
+        if (nz >= source.length || ny >= source[0].length || nx >= source[0][0].length) return
+        if (source[nz][ny][nx] === '#') activeCount++
+      })
+    })
+  })
+  return activeCount
+}
+
+const activeCount = (source) => {
+  return _.sumBy(source, (plane) => {
+    return _.sumBy(plane, (row) => {
+      return _.sumBy(row, (cube) => cube === '#' ? 1 : 0)
+    })
+  })
+}
+
+const printSource = (source) => {
+  source.forEach((plane) => {
+    plane.forEach((row) => {
+      console.log(row.join(''))
+    })
+    console.log('')
+  })
 }
 
 module.exports = { solve }
--- 2020/day17/part02.js
+++ 2020/day17/part02.js
@@ -1,7 +1,99 @@
 const _ = require('lodash')
 
 const solve = (data) => {
-  console.log('This puzzle has not been attempted')
+  const source = parse(data)
+  _.times(6, () => {
+    addSpace(source)
+    boot(source)
+  })
+  console.log(activeCount(source))
+}
+
+const parse = (data) => {
+  return [[data.map((row) => row.split(''))]]
+}
+
+const addSpace = (source) => {
+  source.forEach((box) => {
+    box.forEach((plane) => {
+      plane.forEach((row) => {
+        row.unshift('.')
+        row.push('.')
+      })
+      const rowLength = plane.length + 2
+      plane.unshift(_.times(rowLength, _.constant('.')))
+      plane.push(_.times(rowLength, _.constant('.')))
+    })
+    const planeWidth = box[0][0].length
+    const planeLength = box[0].length
+    box.unshift(_.times(planeLength, () => _.times(planeWidth, _.constant('.'))))
+    box.push(_.times(planeLength, () => _.times(planeWidth, _.constant('.'))))
+  })
+  const boxWidth = source[0][0][0].length
+  const boxHeight = source[0][0].length
+  const boxDepth = source[0].length
+  source.unshift(_.times(boxDepth, () => _.times(boxHeight, () => _.times(boxWidth, _.constant('.')))))
+  source.push(_.times(boxDepth, () => _.times(boxHeight, () => _.times(boxWidth, _.constant('.')))))
+}
+
+const boot = (source) => {
+  const oldSource = _.cloneDeep(source)
+  oldSource.forEach((box, w) => {
+    box.forEach((plane, z) => {
+      plane.forEach((row, y) => {
+        row.forEach((cube, x) => {
+          const activeNeighborCount = activeNeighbors(oldSource, x, y, z, w)
+          if (cube === '.') {
+            source[w][z][y][x] = activeNeighborCount === 3 ? '#' : '.'
+          } else {
+            source[w][z][y][x] = [2, 3].includes(activeNeighborCount) ? '#' : '.'
+          }
+        })
+      })
+    })
+  })
+}
+
+const activeNeighbors = (source, x, y, z, w) => {
+  let activeCount = 0
+  const deltas = [-1, 0, 1]
+  deltas.forEach((dw) => {
+    deltas.forEach((dz) => {
+      deltas.forEach((dy) => {
+        deltas.forEach((dx) => {
+          if (dw === 0 && dz === 0 && dy === 0 && dx === 0) return
+          const nw = w + dw
+          const nz = z + dz
+          const ny = y + dy
+          const nx = x + dx
+          if (nw < 0 || nz < 0 || ny < 0 || nx < 0) return
+          if (nw >= source.length || nz >= source[0].length) return
+          if (ny >= source[0][0].length || nx >= source[0][0][0].length) return
+          if (source[nw][nz][ny][nx] === '#') activeCount++
+        })
+      })
+    })
+  })
+  return activeCount
+}
+
+const activeCount = (source) => {
+  return _.sumBy(source, (box) => {
+    return _.sumBy(box, (plane) => {
+      return _.sumBy(plane, (row) => {
+        return _.sumBy(row, (cube) => cube === '#' ? 1 : 0)
+      })
+    })
+  })
+}
+
+const printBox = (box) => {
+  box.forEach((plane) => {
+    plane.forEach((row) => {
+      console.log(row.join(''))
+    })
+    console.log('')
+  })
 }
 
 module.exports = { solve }
