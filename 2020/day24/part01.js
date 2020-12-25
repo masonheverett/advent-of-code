95dd29e9aee3196d614f058c9d7fdebad4d8cf6c
Mason Everett <masonheverett@gmail.com>
2020-12-25 12:48:33 -0500

    Day 24 part 1

--- 2020/day24/part01.js
+++ 2020/day24/part01.js
@@ -1,7 +1,37 @@
 const _ = require('lodash')
 
 const solve = (data) => {
-  console.log('This puzzle has not been attempted')
+  const pairs = parse(data)
+  console.log(countBlackTiles(pairs))
+}
+
+const parse = (data) => {
+  return data.map((line, i) => {
+    let ew = 0
+    let ns = 0
+    let index = 0
+    while (index < line.length) {
+      const move = ['e', 'w'].includes(line[index]) ? line.substr(index, 1) : line.substr(index, 2)
+      if (['e', 'w'].includes(move)) {
+        ew += move === 'e' ? 1 : -1
+        index++
+      } else {
+         ns += move[0] === 'n' ? 1 : -1
+         ew += move[1] === 'e' ? 0.5 : -0.5
+         index += 2
+      }
+    }
+    return `${ew}|${ns}`
+  })
+}
+
+const countBlackTiles = (pairs) => {
+  const blackTiles = new Set()
+  pairs.forEach((pair) => {
+    if (blackTiles.has(pair)) blackTiles.delete(pair)
+    else blackTiles.add(pair)
+  })
+  return blackTiles.size
 }
 
 module.exports = { solve }
