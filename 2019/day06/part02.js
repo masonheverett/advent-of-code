7c4dfb9073e32a1703ff37439315079811bb00cd
Mason Everett <masonheverett@protonmail.com>
2021-11-24 10:25:23 -0500

    code cleanup 2019 06

--- 2019/day06/part02.js
+++ 2019/day06/part02.js
@@ -1,99 +1,78 @@
-import _ from 'lodash';
-import os from 'os';
-import TreeModel from 'tree-model';
-import treeify from 'treeify';
+import _ from 'lodash'
+import os from 'os'
+import TreeModel from 'tree-model'
 
-var firstRun = true;
+let firstRun = true
 
 export const solve = (data) => {
-  data = data.split(os.EOL);
+  data = data.split(os.EOL)
 
-  var tree = new TreeModel();
-  var root;
-
-  var reprocess = [];
-  var prevReprocess = 0;
+  const tree = new TreeModel()
+  let root
+  let reprocess = []
+  let prevReprocess = 0
 
   for (let i = 0; i < data.length; i++) {
-    let line =  data[i].split(')');
-    if(line.length > 1) {
-      console.log(line[1] + " orbits " + line[0]);
+    let line = data[i].split(')')
+    if (line.length > 1) {
 
-      if(i == 0 && firstRun == true) {
-        root = tree.parse({id: line[0], children: [{id: line[1]}]});
-        firstRun = false;
+      if (i === 0 && firstRun === true) {
+        root = tree.parse({ id: line[0], children: [{ id: line[1] }] })
+        firstRun = false
       } else {
-        //if the ndoe exists
-        var nodeFound = root.first(function (node) {
-          return node.model.id === line[0];
-        });
-        if(nodeFound) {
-          var newNode = tree.parse({id: line[1]});
-          nodeFound.addChild(newNode);
-          console.log('node found and added as child: ' + line[1] + ' i: ' + i);
+        // if the ndoe exists
+        let nodeFound = root.first((node) => {
+          return node.model.id === line[0]
+        })
+        if (nodeFound) {
+          var newNode = tree.parse({ id: line[1] })
+          nodeFound.addChild(newNode)
         } else {
-          console.log("parent node not found with id: " + line[0] + ' pushing to reprocess pile');
-          console.log("working on item: " + i + " with a reprocess size of: " + reprocess.length);
-          reprocess.push(data[i]);
+          reprocess.push(data[i])
         }
       }
 
     }
 
-    if(i == data.length - 1) {
-      console.log('we are at then end of the road, let\'s reprocess the lines not affected');
-      data = reprocess;
-      reprocess = [];
-      i = -1;
+    if (i === data.length - 1) {
+      data = reprocess
+      reprocess = []
+      i = -1
 
-      if(prevReprocess == data.length) {
-        console.log('there are abandoned nodes so breaking');
-        break;
-      }
-      prevReprocess = data.length;
-      console.log('nodes that have no parents: ' + data.length + '\n\n\n');
+      if (prevReprocess === data.length) break
+
+      prevReprocess = data.length
     }
   }
 
-  let directIndirectCounter = 0;
+  let directIndirectCounter = 0
 
-  //this only works if single indirect paths since it does a pre breadth first search
-  root.walk({strategy: 'post'}, function (node) {
-    if(node.isRoot()) {
-      console.log('root node, no routes');
-    } else {
-      console.log("calculating orbits for: " + node.model.id + " as " + eval(node.getPath().length-1));
-      directIndirectCounter += node.getPath().length-1;
+  // this only works if single indirect paths since it does a pre breadth first search
+  root.walk({ strategy: 'post' }, (node) => {
+    if (!node.isRoot()) {
+      directIndirectCounter += node.getPath().length - 1
     }
-  });
+  })
 
-  console.log(directIndirectCounter);
-
-
-  console.log(
-      treeify.asTree(root.model, true, true)
-  );
+  console.log(directIndirectCounter)
 
   var youNode = root.first(function (node) {
-    return node.model.id === 'YOU';
-  });
-  var youPath = youNode.getPath();
+    return node.model.id === 'YOU'
+  })
+  var youPath = youNode.getPath()
 
-  console.log("Paths of YOU node\n\n\n");
-  for(j=0; j < youPath.length; j++) {
-    console.log(" " + youPath[j].model.id + ",");
+  console.log("Paths of YOU node\n\n\n")
+  for (let j = 0; j < youPath.length; j++) {
+    console.log(" " + youPath[j].model.id + ",")
   }
 
-  var sanNode = root.first(function (node) {
-    return node.model.id === 'SAN';
-  });
-  var sanPath = sanNode.getPath();
+  var sanNode = root.first((node) => {
+    return node.model.id === 'SAN'
+  })
+  var sanPath = sanNode.getPath()
 
-  console.log("Paths of SAN node\n\n\n");
-  for(j=0; j < sanPath.length; j++) {
-    console.log(" " + sanPath[j].model.id + ",");
+  console.log("Paths of SAN node\n\n\n")
+  for (let j = 0; j < sanPath.length; j++) {
+    console.log(" " + sanPath[j].model.id + ",")
   }
-
-
-
-};
+}
