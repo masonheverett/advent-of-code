76e6d7ed474fd2636318d1093ef1edcce84f906c
Tim Chabot <tim.chabot@credera.com>
2021-12-01 09:34:25 -0600

    Cleaned up part 1

--- 2021/day01/part01.js
+++ 2021/day01/part01.js
@@ -3,7 +3,6 @@ import _ from 'lodash'
 export const solve = (data) => {
   var numberData = data.map(x => Number(x));
   var count = 0;
-  console.log('data length is: ' + numberData.length);
 
   for(var i=0; i+1 < numberData.length; i++) {
     if(numberData[i+1] > numberData[i]) {
