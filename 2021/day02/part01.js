696bc0c06509bae4199088561dd0bc5a6dac04b4
Tim Chabot <tim.chabot@credera.com>
2021-12-02 07:30:01 -0600

    Day 2, Parts 1 & 2 solutions

--- 2021/day02/part01.js
+++ 2021/day02/part01.js
@@ -1,5 +1,28 @@
 import _ from 'lodash'
 
+const FORWARD = 'forward'
+const UP = 'up'
+const DOWN = 'down'
+
+let pos = { x: 0, y: 0 }
+
 export const solve = (data) => {
-  console.log('Hello, I am day 02 part 01. Solve me, please!')
+  _.forEach(data, (inst) => {
+    const commands = inst.split(' ')
+    const dist = parseInt(commands[1])
+    switch (commands[0]) {
+      case FORWARD:
+        pos.x += dist
+        break
+      case UP:
+        pos.y -= dist
+        break
+      case DOWN:
+        pos.y +=dist
+        break
+    }
+  })
+  console.log(pos)
+  const multipliedDistances = Math.abs(pos.x) * Math.abs(pos.y)
+  console.log(multipliedDistances)
 }
--- 2021/day02/part02.js
+++ 2021/day02/part02.js
@@ -1,5 +1,30 @@
 import _ from 'lodash'
 
+const FORWARD = 'forward'
+const UP = 'up'
+const DOWN = 'down'
+const AIM = 'aim'
+
+let pos = { x: 0, y: 0, aim: 0}
+
 export const solve = (data) => {
-  console.log('Hello, I am day 02 part 02. Solve me, please!')
+  _.forEach(data, (inst) => {
+    const commands = inst.split(' ')
+    const dist = parseInt(commands[1])
+    switch (commands[0]) {
+      case FORWARD:
+        pos.x += dist
+        pos.y += (pos.aim * dist)
+        break
+      case UP:
+        pos.aim -= dist
+        break
+      case DOWN:
+        pos.aim += dist
+        break
+    }
+  })
+  console.log(pos)
+  const multipliedDistances = Math.abs(pos.x) * Math.abs(pos.y)
+  console.log(multipliedDistances)
 }
