e75dae0ace5d251bf7a3b4d11c4db0a348342abc
Tim Chabot <tim.chabot@credera.com>
2021-12-03 08:05:36 -0600

    Day 3, Part 1 solution

--- 2021/day03/part01.js
+++ 2021/day03/part01.js
@@ -1,5 +1,35 @@
 import _ from 'lodash'
 
 export const solve = (data) => {
-  console.log('Hello, I am day 03 part 01. Solve me, please!')
+  var gamma = ''
+  var epsilon = ''
+  var decGamma = 0
+  var decEpsilon = 0
+  var zeroCount = 0
+  var oneCount = 0
+  var lineLength = data[0].length
+
+  console.log('line length: ' + lineLength)
+  for(var pos=0; pos<lineLength; pos++) {
+    for(var line=0; line<data.length; line++) {
+      var c = data[line].charAt(pos)
+      c == '0' ? zeroCount++ : oneCount++
+    }
+
+    if(zeroCount > oneCount) {
+      gamma += '0'
+      epsilon += '1'
+    }
+    else {
+      gamma += '1'
+      epsilon += '0'
+    }
+    zeroCount = 0
+    oneCount = 0
+  }
+  console.log('Gamma: ' + gamma);
+  console.log('Epsilon: ' + epsilon);
+  decGamma = parseInt(gamma, 2)
+  decEpsilon = parseInt(epsilon, 2)
+  console.log(decGamma * decEpsilon)
 }
