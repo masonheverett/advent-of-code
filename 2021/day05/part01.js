7233da61dce028c696a23bfdcbd3082abcc24721
Mason Everett <masonheverett@protonmail.com>
2021-12-05 10:56:30 -0500

    Solve 2021 day 05 part 01

--- 2021/day05/part01.js
+++ 2021/day05/part01.js
@@ -1,5 +1,34 @@
 import _ from 'lodash'
 
 export const solve = (data) => {
-  console.log('Hello, I am day 05 part 01. Solve me, please!')
+  const lines = parseLines(data)
+  const points = {}
+  lines.forEach(line => {
+    if (line[0][0] === line[1][0]) {
+      const x = line[0][0]
+      const min = _.min([line[0][1], line[1][1]])
+      const max = _.max([line[0][1], line[1][1]])
+      for (let y = min; y <= max; y++) {
+        if (points[`${x},${y}`] === undefined) points[`${x},${y}`] = 1
+        else points[`${x},${y}`]++
+      }
+    } else {
+      const y = line[0][1]
+      const min = _.min([line[0][0], line[1][0]])
+      const max = _.max([line[0][0], line[1][0]])
+      for (let x = min; x <= max; x++) {
+        if (points[`${x},${y}`] === undefined) points[`${x},${y}`] = 1
+        else points[`${x},${y}`]++
+      }
+    }
+  })
+  console.log(_.filter(_.entries(points), counter => counter[1] > 1).length)
+}
+
+const parseLines = (data) => {
+  return data.map(line => {
+    return line.split(' -> ').map(point => point.split(',').map(Number))
+  }).filter(line => {
+    return line[0][0] === line[1][0] || line[0][1] === line[1][1]
+  })
 }
