49d3b7d65c5ce0aef60896f134a350fb0dc1d308
Quintin Roper <quintin.roper@angi.com>
2021-12-06 10:10:00 -0700

    2021 Day 6 Part 2

--- 2021/day06/part02.js
+++ 2021/day06/part02.js
@@ -1,5 +1,24 @@
 import _ from 'lodash'
 
 export const solve = (data) => {
-  console.log('Hello, I am day 06 part 02. Solve me, please!')
+  const numericData = data[0].split(',').map(Number)
+  var buckets = parseData(numericData)
+
+  for(var i=0; i<256; i++) {
+    const temp = buckets[0]
+    for (var j=0; j<8; j++) {
+      buckets[j] = buckets[j + 1]
+    }
+    buckets[8] = temp
+    buckets[6] += temp
+  }
+  console.log(_.sum(buckets))
+}
+
+const parseData = (data) => {
+  var buckets = _.times(9, _.constant(0))
+  _.forEach(data, (fish) => {
+    buckets[fish]++
+  })
+  return buckets
 }
