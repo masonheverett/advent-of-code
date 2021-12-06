9a79cca6475e867743372705dbea20965da4d332
Tim Chabot <tim.chabot@credera.com>
2021-12-06 08:49:25 -0600

    Day 6, part 1 solution

--- 2021/day06/part01.js
+++ 2021/day06/part01.js
@@ -1,5 +1,18 @@
 import _ from 'lodash'
 
 export const solve = (data) => {
-  console.log('Hello, I am day 06 part 01. Solve me, please!')
+  var numericData = data[0].split(',').map(Number)
+
+  for(var i=0; i<80; i++) {
+    _.forEach(numericData, (number, index) => {
+      if(number == 0) {
+        numericData.push(8)
+        numericData[index] = 6
+      }
+      else {
+        numericData[index]--
+      }
+    })
+  }
+  console.log(numericData.length)
 }
