c0436fa4d37896f41ca93760c13721a973451074
Mason Everett <masonheverett@protonmail.com>
2021-12-07 09:50:12 -0500

    Solve 2021 day 07 parts 01 and 02

--- 2021/day07/part01.js
+++ 2021/day07/part01.js
@@ -1,5 +1,18 @@
 import _ from 'lodash'
 
 export const solve = (data) => {
-  console.log('Hello, I am day 07 part 01. Solve me, please!')
+  const positions = data[0].split(',').map(Number)
+  const min = _.min(positions)
+  const max = _.max(positions)
+  let leastCost
+  for (let i = min; i <= max; i++) {
+    let cost = 0
+    for (let j = 0; j < positions.length; j++) {
+      cost += Math.abs(i - positions[j])
+    }
+    if (i === 0 || cost < leastCost) {
+      leastCost = cost
+    }
+  }
+  console.log(leastCost)
 }
