97c7b3143a1cc5bf28c807d78c27c6d0f1b6297c
Tim Chabot <tim.chabot@credera.com>
2021-12-08 11:24:02 -0600

    Day 8, part 1 solution

--- 2021/day08/part01.js
+++ 2021/day08/part01.js
@@ -1,5 +1,31 @@
 import _ from 'lodash'
 
 export const solve = (data) => {
-  console.log('Hello, I am day 08 part 01. Solve me, please!')
+  const outputValues = []
+  var count = 0;
+  
+  _.forEach(data, line => {
+    outputValues.push(getOutputValues(line))
+  })
+
+  _.forEach(outputValues, digitSequence => {
+    _.forEach(digitSequence, sequence => {
+      if(doesOutputMatchUnique(sequence)) {
+        count++
+      }
+    })
+  })
+  
+  console.log(count)
+}
+
+const getOutputValues = (line) => {
+  var pipeIndex = line.indexOf('|')
+  var output = line.slice(pipeIndex+2).split(' ')
+  return output
+}
+
+const doesOutputMatchUnique = (s) => {
+  if(s.length == 2 || s.length == 4 || s.length == 3 || s.length == 7) return true
+  return false
 }
