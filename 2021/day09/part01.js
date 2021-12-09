7e9c560e3a38cb05d96dfab88763098f33a75efb
Mason Everett <masonheverett@protonmail.com>
2021-12-09 11:27:36 -0500

    Solve 2021 day 09 part 01

--- 2021/day09/part01.js
+++ 2021/day09/part01.js
@@ -1,5 +1,21 @@
 import _ from 'lodash'
 
 export const solve = (data) => {
-  console.log('Hello, I am day 09 part 01. Solve me, please!')
+  const grid = data.map(line => line.split('').map(Number))
+  let riskLevel = 0
+
+  for (let row = 0; row < grid.length; row++) {
+    for (let col = 0; col < grid[row].length; col++) {
+      if (
+        (row === 0 || grid[row][col] < grid[row - 1][col]) &&
+        (col === grid[row].length - 1 || grid[row][col] < grid[row][col + 1]) &&
+        (row === grid.length - 1 || grid[row][col] < grid[row + 1][col]) &&
+        (col === 0 || grid[row][col] < grid[row][col - 1])
+      ) {
+        riskLevel += (1 + grid[row][col])
+      }
+    }
+  }
+
+  console.log(riskLevel)
 }
