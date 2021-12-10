be8b6c042f7a16b845401e7c37b55cd528e6caac
Mason Everett <masonheverett@protonmail.com>
2021-12-10 12:09:40 -0500

    Solve 2021 day 10 part 02

--- 2021/day10/part02.js
+++ 2021/day10/part02.js
@@ -1,5 +1,54 @@
 import _ from 'lodash'
 
 export const solve = (data) => {
-  console.log('Hello, I am day 10 part 02. Solve me, please!')
+  const incompleteLineStacks = []
+  data.forEach(line => {
+    const lineArr = line.split('')
+    const openBraces = []
+    for (let i = 0; i < lineArr.length; i++) {
+      const brace = lineArr[i]
+      if (isOpenBrace(brace)) {
+        openBraces.push(brace)
+      } else {
+        if (_.last(openBraces) === matchingBrace(brace)) openBraces.pop()
+        else return
+      }
+    }
+    incompleteLineStacks.push(openBraces)
+  })
+  console.log(_.sortBy(incompleteLineStacks.map(completionStringScore))[(incompleteLineStacks.length - 1) / 2])
+}
+
+const isOpenBrace = (brace) => {
+  return brace === '(' || brace === '[' || brace === '{' || brace === '<'
+}
+
+const matchingBrace = (brace) => {
+  return {
+    '(': ')',
+    ')': '(',
+    '[': ']',
+    ']': '[',
+    '{': '}',
+    '}': '{',
+    '<': '>',
+    '>': '<',
+  }[brace]
+}
+
+const braceScore = (brace) => {
+  return {
+    ')': 1,
+    ']': 2,
+    '}': 3,
+    '>': 4,
+  }[brace]
+}
+
+const completionStringScore = (openBraces) => {
+  return _.reverse(openBraces)
+    .map(matchingBrace)
+    .reduce((total, nextBrace) => {
+      return total * 5 + braceScore(nextBrace)
+    }, 0)
 }
