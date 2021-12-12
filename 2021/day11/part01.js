0b4b3ef1ab0d5d139fc73734776366701009c74d
Tim Chabot <tim.chabot@credera.com>
2021-12-11 21:09:03 -0600

    Day 11, part 1 solution

--- 2021/day11/part01.js
+++ 2021/day11/part01.js
@@ -1,5 +1,53 @@
 import _ from 'lodash'
 
 export const solve = (data) => {
-  console.log('Hello, I am day 11 part 01. Solve me, please!')
+  const grid = data.map(line => line.split('').map(Number))
+  const maxSteps = 100
+  var flashCount = 0
+  
+  for(let steps = 0; steps < maxSteps; steps++) {
+    stepTransform(grid)
+    flashCount += processFlashes(grid)
+  }
+  console.log(grid)
+  console.log(flashCount)
+}
+
+const stepTransform = (grid) => {
+  for (let row = 0; row < grid.length; row++) {
+    for (let col = 0; col < grid[row].length; col++) {
+      grid[row][col]++
+    }
+  }
+}
+
+const processFlashes = (grid) => {
+  var flashCount = 0
+  while(!isGridComplete(grid)) {
+    for (let row = 0; row < grid.length; row++) {
+      for (let col = 0; col < grid[row].length; col++) {
+        if(grid[row][col] > 9) {
+          for(let flashRow = row-1; flashRow <= row+1; flashRow++) {
+            for(let flashCol = col-1; flashCol <= col+1; flashCol++) {
+              if((flashRow >= 0 && flashCol >= 0) && (flashRow < grid.length && flashCol < grid[row].length) && grid[flashRow][flashCol] > 0) {
+                grid[flashRow][flashCol]++
+              } 
+            }
+          }
+          flashCount++
+          grid[row][col] = 0
+        }
+      }
+    }
+  }
+  return flashCount
+}
+
+const isGridComplete = (grid) => {
+  for (let row = 0; row < grid.length; row++) {
+    if(grid[row].find(element => element > 9) !== undefined) {
+      return false;
+    }
+  }
+  return true
 }
