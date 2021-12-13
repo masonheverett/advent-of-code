e033701adb02052456d7e4f20ea1fa5e88a158c4
Mason Everett <masonheverett@protonmail.com>
2021-12-13 17:06:54 -0500

    Solve 2021 day 12 part 01

--- 2021/day12/part01.js
+++ 2021/day12/part01.js
@@ -1,5 +1,32 @@
 import _ from 'lodash'
 
+let pathCount = 0
+
 export const solve = (data) => {
-  console.log('Hello, I am day 12 part 01. Solve me, please!')
+  const graph = {}
+  data.forEach(line => {
+    const edge = line.split('-')
+    if (graph[edge[0]] === undefined) {
+      graph[edge[0]] = { neighbors: [], blocked: false }
+    }
+    graph[edge[0]].neighbors.push(edge[1])
+    if (graph[edge[1]] === undefined) {
+      graph[edge[1]] = { neighbors: [], blocked: false }
+    }
+    graph[edge[1]].neighbors.push(edge[0])
+  })
+  findPaths(graph, 'start')
+  console.log(pathCount)
+}
+
+const findPaths = (graph, nextNode) => {
+  if (graph[nextNode].blocked) return
+  if (nextNode === 'end') {
+    pathCount++
+    return
+  }
+  if (nextNode.toLowerCase() === nextNode) graph[nextNode].blocked = true
+  graph[nextNode].neighbors.forEach(neighbor => {
+    findPaths(_.cloneDeep(graph), neighbor)
+  })
 }
