a4e782235a05cd46052352335be8ab0b6e4cd895
Mason Everett <masonheverett@protonmail.com>
2021-12-15 15:31:12 -0500

    Clean up

--- 2021/day15/part02.js
+++ 2021/day15/part02.js
@@ -96,9 +96,9 @@ class PriorityQueue {
       this.collection.push(element)
     } else {
       let added = false
-      for (let i = 1; i <= this.collection.length; i++) {
-        if (element[1] < this.collection[i - 1][1]) {
-          this.collection.splice(i - 1, 0, element)
+      for (let i = 0; i < this.collection.length; i++) {
+        if (element[1] < this.collection[i][1]) {
+          this.collection.splice(i, 0, element)
           added = true
           break
         }
