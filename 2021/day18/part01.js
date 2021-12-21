73d8455abec5ede1d779e3cd94937745efb9d637
Mason Everett <masonheverett@protonmail.com>
2021-12-21 00:15:14 -0500

    Resolving 2021 18 01 with tree

--- 2021/day18/part01.js
+++ 2021/day18/part01.js
@@ -1,89 +1,11 @@
 import _ from 'lodash'
 
 export const solve = (data) => {
-  console.log(magnitude(data.reduce((prev, next) => {
-    let num = `[${prev},${next}]`
-    // console.log(`after addition: ${num}`)
-    let explodeIndex = findExplodeIndex(num)
-    while (explodeIndex > -1) {
-      num = explode(num, explodeIndex)
-      // console.log(`after explode:  ${num}`)
-      explodeIndex = findExplodeIndex(num)
-      while (explodeIndex < 0) {
-        const splitIndex = findSplitIndex(num)
-        if (splitIndex > -1) {
-          num = split(num, splitIndex)
-          // console.log(`after split:    ${num}`)
-          explodeIndex = findExplodeIndex(num)
-        } else break
-      }
-    }
-    return num
-  })))
-}
-
-const magnitude = (num) => {
-  while (!isNumber(num.charAt(0))) {
-    num = processLeaves(num)
-  }
-  return Number(num)
-}
-
-const processLeaves = (num) => {
-  return num.replace(/\[(\d+),(\d+)\]/g, (match, p1, p2) => (3 * Number(p1) + 2 * Number(p2)).toString())
-}
-
-const findExplodeIndex = (num) => {
-  let nestCount = 0
-  for (let i = 0; i < num.length; i++) {
-    if (num.charAt(i) === '[') nestCount++
-    else if (num.charAt(i) === ']') nestCount--
-    else if (nestCount > 4) return i
-  }
-  return -1
-}
-
-const explode = (num, index) => {
-  const leftNum = readNum(num.substring(index))
-  const rightNum = readNum(num.substring(index + leftNum.toString().length + 1))
-  let leftBraceIndex = index - 1
-  let rightBraceIndex = leftBraceIndex + leftNum.toString().length + rightNum.toString().length + 2
-  for (let i = leftBraceIndex; i >= 0; i--) {
-    if (isNumber(num.charAt(i))) {
-      while (i >= 0 && isNumber(num.charAt(i - 1))) i--
-      const toReplace = readNum(num.substring(i))
-      const newNum = leftNum + toReplace
-      const indexShift = (newNum.toString().length - toReplace.toString().length)
-      leftBraceIndex += indexShift
-      rightBraceIndex += indexShift
-      num = `${num.substring(0, i)}${newNum}${num.substring(i + toReplace.toString().length)}`
-      break
-    }
-  }
-  for (let i = rightBraceIndex + 1; i < num.length; i++) {
-    if (isNumber(num.charAt(i))) {
-      const toReplace = readNum(num.substring(i))
-      const newNum = rightNum + toReplace
-      num = num.substring(0, i) + newNum + num.substring(i + toReplace.toString().length)
-      break
-    }
-  }
-  num = `${num.substring(0, leftBraceIndex)}0${num.substring(rightBraceIndex + 1)}`
-  return num
-}
-
-const findSplitIndex = (num) => {
-  for (let i = 0; i < num.length; i++) {
-    if (isNumber(num.charAt(i)) && readNum(num.substring(i)) > 9) return i
-  }
-  return -1
-}
-
-const split = (num, index) => {
-  const toReplace = readNum(num.substring(index))
-  const firstNum = Math.floor(toReplace / 2)
-  const secondNum = Math.ceil(toReplace / 2)
-  return `${num.substring(0, index)}[${firstNum},${secondNum}]${num.substring(index + toReplace.toString().length)}`
+  console.log(
+    data.map(line => new TreeNode().fromString(line))
+      .reduce((prev, next) => new TreeNode().withChildren(prev, next).reduce())
+      .magnitude()
+  )
 }
 
 const readNum = (str) => {
@@ -92,4 +14,127 @@ const readNum = (str) => {
 
 const isNumber = (char) => {
   return ![',', '[', ']'].includes(char)
+}
+
+const switchLastFork = (str, from, to) => {
+  const lastIndex = _.lastIndexOf(str.split(''), from)
+  return `${str.substring(0, lastIndex)}${to}`
+}
+
+class TreeNode {
+  fromString(str) {
+    if (isNumber(str.charAt(0))) {
+      this.value = readNum(str)
+    } else {
+      const comma = this.findCommaIndex(str)
+      this.left = new TreeNode().fromString(str.substring(1, comma))
+      this.right = new TreeNode().fromString(str.substring(comma + 1))
+    }
+    return this
+  }
+
+  withValue(value) {
+    this.value = value
+    return this
+  }
+
+  withChildren(left, right) {
+    this.left = left
+    this.right = right
+    return this
+  }
+
+  toString() {
+    if (this.value !== undefined) return this.value.toString()
+    return `[${this.left.toString()},${this.right.toString()}]`
+  }
+
+  navigateTo(directions) {
+    let nodeToReplace = this
+    for (let i = 0; i < directions.length; i++) {
+      if (directions.charAt(i) === 'L') nodeToReplace = nodeToReplace.left
+      else nodeToReplace = nodeToReplace.right
+    }
+    return nodeToReplace
+  }
+
+  findCommaIndex(str) {
+    let countOpen = 0
+    for (let i = 0; i < str.length; i++) {
+      if (str.charAt(i) === '[') countOpen++
+      else if (str.charAt(i) === ']') countOpen--
+      else if (str.charAt(i) === ',' && countOpen === 1) return i
+    }
+  }
+
+  reduce() {
+    let toExplode = this.firstToExplode('')
+    while (toExplode !== undefined) {
+      this.explode(toExplode)
+      toExplode = this.firstToExplode('')
+      while (toExplode === undefined) {
+        let toSplit = this.firstToSplit('')
+        if (toSplit !== undefined) {
+          this.split(toSplit)
+          toExplode = this.firstToExplode('')
+        } else break
+      }
+    }
+    return this
+  }
+
+  firstToExplode(directions) {
+    if (this.value !== undefined) {
+      if (directions.length > 4) return directions.substring(0, directions.length - 1)
+      return undefined
+    }
+    const left = this.left.firstToExplode(directions + 'L')
+    if (left !== undefined) return left
+    const right = this.right.firstToExplode(directions + 'R')
+    if (right !== undefined) return right
+    return undefined
+  }
+
+  explode(directions) {
+    let nodeToReplace = this.navigateTo(directions)
+    if (_.includes(directions, 'R')) {
+      let leftNode = this.navigateTo(switchLastFork(directions, 'R', 'L'))
+      while (leftNode.value === undefined) leftNode = leftNode.right
+      leftNode.value += nodeToReplace.left.value
+    }
+    if (_.includes(directions, 'L')) {
+      let rightNode = this.navigateTo(switchLastFork(directions, 'L', 'R'))
+      while (rightNode.value === undefined) rightNode = rightNode.left
+      rightNode.value += nodeToReplace.right.value
+    }
+    nodeToReplace.left = undefined
+    nodeToReplace.right = undefined
+    nodeToReplace.value = 0
+    return this
+  }
+
+  firstToSplit(directions) {
+    if (this.value !== undefined) {
+      if (this.value > 9) return directions
+      return undefined
+    }
+    const left = this.left.firstToSplit(directions + 'L')
+    if (left !== undefined) return left
+    const right = this.right.firstToSplit(directions + 'R')
+    if (right !== undefined) return right
+    return undefined
+  }
+
+  split(directions) {
+    const nodeToReplace = this.navigateTo(directions)
+    nodeToReplace.left = new TreeNode().withValue(Math.floor(nodeToReplace.value / 2))
+    nodeToReplace.right = new TreeNode().withValue(Math.ceil(nodeToReplace.value / 2))
+    nodeToReplace.value = undefined
+    return this
+  }
+
+  magnitude() {
+    if (this.value !== undefined) return this.value
+    return 3 * this.left.magnitude() + 2 * this.right.magnitude()
+  }
 }
--- 2021/day18/part02.js
+++ 2021/day18/part02.js
@@ -1,5 +1,140 @@
 import _ from 'lodash'
 
 export const solve = (data) => {
-  console.log('Hello, I am day 18 part 02. Solve me, please!')
+  console.log(
+    data.map(line => new TreeNode().fromString(line))
+      .reduce((prev, next) => new TreeNode().withChildren(prev, next).reduce())
+      .magnitude()
+  )
+}
+
+const readNum = (str) => {
+  return Number(_.takeWhile(str.split(''), isNumber).join(''))
+}
+
+const isNumber = (char) => {
+  return ![',', '[', ']'].includes(char)
+}
+
+const switchLastFork = (str, from, to) => {
+  const lastIndex = _.lastIndexOf(str.split(''), from)
+  return `${str.substring(0, lastIndex)}${to}`
+}
+
+class TreeNode {
+  fromString(str) {
+    if (isNumber(str.charAt(0))) {
+      this.value = readNum(str)
+    } else {
+      const comma = this.findCommaIndex(str)
+      this.left = new TreeNode().fromString(str.substring(1, comma))
+      this.right = new TreeNode().fromString(str.substring(comma + 1))
+    }
+    return this
+  }
+
+  withValue(value) {
+    this.value = value
+    return this
+  }
+
+  withChildren(left, right) {
+    this.left = left
+    this.right = right
+    return this
+  }
+
+  toString() {
+    if (this.value !== undefined) return this.value.toString()
+    return `[${this.left.toString()},${this.right.toString()}]`
+  }
+
+  navigateTo(directions) {
+    let nodeToReplace = this
+    for (let i = 0; i < directions.length; i++) {
+      if (directions.charAt(i) === 'L') nodeToReplace = nodeToReplace.left
+      else nodeToReplace = nodeToReplace.right
+    }
+    return nodeToReplace
+  }
+
+  findCommaIndex(str) {
+    let countOpen = 0
+    for (let i = 0; i < str.length; i++) {
+      if (str.charAt(i) === '[') countOpen++
+      else if (str.charAt(i) === ']') countOpen--
+      else if (str.charAt(i) === ',' && countOpen === 1) return i
+    }
+  }
+
+  reduce() {
+    let toExplode = this.firstToExplode('')
+    while (toExplode !== undefined) {
+      this.explode(toExplode)
+      toExplode = this.firstToExplode('')
+      while (toExplode === undefined) {
+        let toSplit = this.firstToSplit('')
+        if (toSplit !== undefined) {
+          this.split(toSplit)
+          toExplode = this.firstToExplode('')
+        } else break
+      }
+    }
+    return this
+  }
+
+  firstToExplode(directions) {
+    if (this.value !== undefined) {
+      if (directions.length > 4) return directions.substring(0, directions.length - 1)
+      return undefined
+    }
+    const left = this.left.firstToExplode(directions + 'L')
+    if (left !== undefined) return left
+    const right = this.right.firstToExplode(directions + 'R')
+    if (right !== undefined) return right
+    return undefined
+  }
+
+  explode(directions) {
+    let nodeToReplace = this.navigateTo(directions)
+    if (_.includes(directions, 'R')) {
+      let leftNode = this.navigateTo(switchLastFork(directions, 'R', 'L'))
+      while (leftNode.value === undefined) leftNode = leftNode.right
+      leftNode.value += nodeToReplace.left.value
+    }
+    if (_.includes(directions, 'L')) {
+      let rightNode = this.navigateTo(switchLastFork(directions, 'L', 'R'))
+      while (rightNode.value === undefined) rightNode = rightNode.left
+      rightNode.value += nodeToReplace.right.value
+    }
+    nodeToReplace.left = undefined
+    nodeToReplace.right = undefined
+    nodeToReplace.value = 0
+    return this
+  }
+
+  firstToSplit(directions) {
+    if (this.value !== undefined) {
+      if (this.value > 9) return directions
+      return undefined
+    }
+    const left = this.left.firstToSplit(directions + 'L')
+    if (left !== undefined) return left
+    const right = this.right.firstToSplit(directions + 'R')
+    if (right !== undefined) return right
+    return undefined
+  }
+
+  split(directions) {
+    const nodeToReplace = this.navigateTo(directions)
+    nodeToReplace.left = new TreeNode().withValue(Math.floor(nodeToReplace.value / 2))
+    nodeToReplace.right = new TreeNode().withValue(Math.ceil(nodeToReplace.value / 2))
+    nodeToReplace.value = undefined
+    return this
+  }
+
+  magnitude() {
+    if (this.value !== undefined) return this.value
+    return 3 * this.left.magnitude() + 2 * this.right.magnitude()
+  }
 }
