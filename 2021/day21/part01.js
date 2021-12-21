74bf629c7c25079b02fb73da55b5356037a137d8
Mason Everett <masonheverett@protonmail.com>
2021-12-21 01:47:43 -0500

    Solve 2021 day 21 part 01

--- 2021/day21/part01.js
+++ 2021/day21/part01.js
@@ -1,5 +1,32 @@
 import _ from 'lodash'
 
 export const solve = (data) => {
-  console.log('Hello, I am day 21 part 01. Solve me, please!')
+  const players = [{
+    position: Number(data[0].substring(28)),
+    score: 0
+  }, {
+    position: Number(data[1].substring(28)),
+    score: 0
+  }]
+  const roll = getRoller()
+  let turns = 0
+  while (players[0].score < 1000 && players[1].score < 1000) {
+    const player = players[turns % 2]
+    const rolls = [roll(), roll(), roll()]
+    const spaces = rolls[0] + rolls[1] + rolls[2]
+    player.position = (player.position + spaces) % 10
+    if (player.position === 0) player.position = 10
+    player.score += player.position
+    turns++
+  }
+  console.log(turns * 3 * players[turns % 2].score)
+}
+
+const getRoller = () => {
+  let nextRoll = 1
+  return () => {
+    const roll = nextRoll
+    nextRoll = (roll % 100) + 1
+    return roll
+  }
 }
