190e565e757d0298e37e171aa4ce70b41918cb2a
Mason Everett <mason@masonheverett.com>
2024-12-05 14:49:31 -0500

    Add comments for day 5

--- 2024/day05/part01.js
+++ 2024/day05/part01.js
@@ -4,11 +4,13 @@ export const solve = (data) => {
   const rules = {}
   const updates = []
   data.forEach(line => {
+    // Parse rules
     if (line.includes('|')) {
       const pair = line.split('|')
       if (rules[pair[0]] === undefined) rules[pair[0]] = new Set()
       rules[pair[0]].add(pair[1])
     }
+    // Parse updates
     if (line.includes(',')) {
       updates.push(line.split(','))
     }
@@ -16,9 +18,11 @@ export const solve = (data) => {
   console.log(_.sum(updates.map(update => {
     for (let i = 0; i < update.length; i++) {
       for (let j = i + 1; j < update.length; j++) {
+        // Check if any pairs are out of order
         if (rules[update[j]] && rules[update[j]].has(update[i])) return 0
       }
     }
+    // For the rest, add up the middle numbers
     return _.toNumber(update[(update.length - 1) / 2])
   })))
 }
--- 2024/day05/part02.js
+++ 2024/day05/part02.js
@@ -4,15 +4,18 @@ export const solve = (data) => {
   const rules = {}
   const updates = []
   data.forEach(line => {
+    // Parse rules
     if (line.includes('|')) {
       const pair = line.split('|')
       if (rules[pair[0]] === undefined) rules[pair[0]] = new Set()
       rules[pair[0]].add(pair[1])
     }
+    // Parse updates
     if (line.includes(',')) {
       updates.push(line.split(','))
     }
   })
+  // Get list of updates that are out of order
   const outOfOrderUpdates = updates.filter(update => {
     for (let i = 0; i < update.length; i++) {
       for (let j = i + 1; j < update.length; j++) {
@@ -22,12 +25,13 @@ export const solve = (data) => {
     return false
   })
   console.log(_.sum(outOfOrderUpdates.map(update => {
-    let i = 0
+    // Sort the updates
     const sorted = update.sort((page1, page2) => {
       if (rules[page1].has(page2)) return -1
       if (rules[page2].has(page1)) return 1
       return 0
     })
+    // Add up the middle numbers
     return _.toNumber(sorted[(update.length - 1) / 2])
   })))
 }
