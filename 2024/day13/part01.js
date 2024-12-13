e4c98c495ed8242463730b1e00bd742a8996658f
Mason Everett <mason@masonheverett.com>
2024-12-13 12:36:58 -0500

    2024 13 02

--- 2024/day13/part01.js
+++ 2024/day13/part01.js
@@ -12,26 +12,26 @@ export const solve = (data) => {
     const by = _.toNumber(chunk[1].match(/Y\+\d+/)[0].substring(2))
     const px = _.toNumber(chunk[2].match(/X=\d+/)[0].substring(2))
     const py = _.toNumber(chunk[2].match(/Y=\d+/)[0].substring(2))
-    // Loop through possibilities for Button A
-    let cheapest = Number.POSITIVE_INFINITY
-    for (let a = 0; a <= MAX_PUSHES; a++) {
-      // Start with X axis
-      const b = (px - (ax * a)) / bx
-      // If B is over 100, skip
-      if (b > 100) continue
-      // If B is less than 0, stop
-      if (b < 0) break
-      // If B is not a whole number, skip
-      if (!_.isInteger(b)) continue
-      // Check Y axis
-      if (ay * a + by * b === py) {
-        const cost = 3 * a + b
-        // Check if cheapest so far
-        if (cost < cheapest) cheapest = cost
-      }
-    }
-    // If there is a valid path, add it to the total
-    if (cheapest < Number.POSITIVE_INFINITY) total += cheapest
+    // Solve for a
+    //   (ax)(a) + (bx)(b) = px :: b = (px - (ax)(a)) / bx
+    //   (ay)(a) + (by)(b) = py :: b = (py - (ay)(a)) / by
+    //   (px - (ax)(a)) / bx = (py - (ay)(a)) / by
+    //   px - (ax)(a) = (bx * (py - (ay)(a))) / by
+    //   by * (px - (ax) (a)) = bx * (py - (ay)(a))
+    //   (by)(px) - (by)(ax)(a) = (bx)(py) - (bx)(ay)(a)
+    //   (bx)(ay)(a) - (by)(ax)(a) + (by)(px) = (bx)(py)
+    //   (bx)(ay)(a) - (by)(ax)(a) = (bx)(py) - (by)(px)
+    //   ((bx)(ay) - (by)(ax))(a) = (bx)(py) - (by)(px)
+    //   a = ((bx)(py) - (by)(px)) / ((bx)(ay) - (by)(ax))
+    const a = ((bx * py) - (by * px)) / ((bx * ay) - (by * ax))
+    // Check max pushes and for integer
+    if (a > MAX_PUSHES || !_.isInteger(a)) return
+    // Solve for b
+    const b = (px - (ax * a)) / bx
+    // Check max pushes
+    if (b > MAX_PUSHES) return
+    // Add to total
+    total += 3 * a + b
   })
   console.log(total)
 }
--- 2024/day13/part02.js
+++ 2024/day13/part02.js
@@ -1,5 +1,34 @@
 import _ from 'lodash'
 
+// Gotta use BigInt thanks to this
+const PRIZE_OFFSET = 10000000000000n
+
 export const solve = (data) => {
-  console.log('This problem has not been attempted yet.')
+  let total = 0n
+  _.chunk(data, 4).forEach(chunk => {
+    // Parse input
+    const ax = BigInt(chunk[0].match(/X\+\d+/)[0].substring(2))
+    const ay = BigInt(chunk[0].match(/Y\+\d+/)[0].substring(2))
+    const bx = BigInt(chunk[1].match(/X\+\d+/)[0].substring(2))
+    const by = BigInt(chunk[1].match(/Y\+\d+/)[0].substring(2))
+    const px = BigInt(chunk[2].match(/X=\d+/)[0].substring(2)) + PRIZE_OFFSET
+    const py = BigInt(chunk[2].match(/Y=\d+/)[0].substring(2)) + PRIZE_OFFSET
+    // Solve for a
+    //   (ax)(a) + (bx)(b) = px :: b = (px - (ax)(a)) / bx
+    //   (ay)(a) + (by)(b) = py :: b = (py - (ay)(a)) / by
+    //   (px - (ax)(a)) / bx = (py - (ay)(a)) / by
+    //   px - (ax)(a) = (bx * (py - (ay)(a))) / by
+    //   by * (px - (ax) (a)) = bx * (py - (ay)(a))
+    //   (by)(px) - (by)(ax)(a) = (bx)(py) - (bx)(ay)(a)
+    //   (bx)(ay)(a) - (by)(ax)(a) + (by)(px) = (bx)(py)
+    //   (bx)(ay)(a) - (by)(ax)(a) = (bx)(py) - (by)(px)
+    //   ((bx)(ay) - (by)(ax))(a) = (bx)(py) - (by)(px)
+    //   a = ((bx)(py) - (by)(px)) / ((bx)(ay) - (by)(ax))
+    const a = ((bx * py) - (by * px)) / ((bx * ay) - (by * ax))
+    // Solve for b
+    const b = (px - (ax * a)) / bx
+    // Verify there was no rounding due to BigInt, add to total
+    if ((ax * a) + (bx * b) === px && (ay * a) + (by * b) === py) total += 3n * a + b
+  })
+  console.log(total)
 }
