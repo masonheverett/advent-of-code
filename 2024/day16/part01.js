8f9ec4ad4c8c4183302da64a8a0494ce62019121
Mason Everett <mason@masonheverett.com>
2024-12-16 13:00:37 -0500

    Data structures for 2024 16 01

--- 2024/day16/part01.js
+++ 2024/day16/part01.js
@@ -1,5 +1,48 @@
 import _ from 'lodash'
 
+const MARKERS = {
+  start: 'S',
+  end: 'E',
+  wall: '#',
+  space: '.',
+  reindeer: 'R',
+  visited: '@'
+}
+
+const DIRECTIONS = {
+  north: [-1, 0],
+  east: [0, 1],
+  south: [1, 0],
+  west: [0, -1]
+}
+
+class Reindeer {
+  constructor(maze, cost, direction, location) {
+    this.maze = maze
+    this.cost = cost
+    this.direction = direction ? direction : DIRECTIONS.east
+    if (location) {
+      this.location = location
+    } else {
+      const rowNdx = this.maze.findIndex(row => row.includes(MARKERS.start))
+      this.location = [rowNdx, this.maze[rowNdx].indexOf(MARKERS.start)]
+    }
+  }
+
+  currentMarker() { return this.maze[this.location[0], this.location[1]] }
+
+  print() {
+    console.log(`Direction: ${this.direction}\nCost: ${this.cost}`)
+    this.maze.forEach((row, r) => {
+      console.log(row.map((spot, c) => {
+        if (this.location[0] === r && this.location[1] === c) return MARKERS.reindeer
+        return spot
+      }).join(''))
+    })
+  }
+}
+
 export const solve = (data) => {
-  console.log('This problem has not been attempted yet.')
+  const reindeer = new Reindeer(data.map(line => line.split('')), 0)
+  reindeer.print()
 }
