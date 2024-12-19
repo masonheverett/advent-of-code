185aa40e9c84ce40da2a9dc51e54738dc34bd11a
Mason Everett <mason@masonheverett.com>
2024-12-19 14:42:42 -0500

    2024 19 02

--- 2024/day19/part01.js
+++ 2024/day19/part01.js
@@ -1,93 +1,31 @@
 import _ from 'lodash'
 
-export class MaxHeap {
-  _heap = []
-
-  cost(node) { return node }
-
-  get size() { return this._heap.length }
-
-  getParentIndex(ndx) { return Math.floor((ndx - 1) / 2) }
-  getLeftChildIndex(ndx) { return 2 * ndx + 1 }
-  getRightChildIndex(ndx) { return 2 * ndx + 2 }
-
-  swap(ndx1, ndx2) { [this._heap[ndx1], this._heap[ndx2]] = [this._heap[ndx2], this._heap[ndx1]] }
-
-  push(node) {
-    this._heap.push(node)
-    this.heapifyUp()
-  }
-
-  heapifyUp() {
-    let ndx = this.size - 1
-    while (ndx > 0) {
-      let pNdx = this.getParentIndex(ndx)
-      if (this.cost(this._heap[pNdx]) < this.cost(this._heap[ndx])) {
-        this.swap(pNdx, ndx)
-        ndx = pNdx
-      } else {
-        break
-      }
-    }
-  }
-
-  pop() {
-    if (this.size === 0) return null
-    if (this.size === 1) return this._heap.pop()
-    const min = this._heap[0]
-    this._heap[0] = this._heap.pop()
-    this.heapifyDown()
-    return min
-  }
-
-  heapifyDown() {
-    let ndx = 0
-    while (this.getLeftChildIndex(ndx) < this.size) {
-      let lChildNdx = this.getLeftChildIndex(ndx)
-      let rChildNdx = this.getRightChildIndex(ndx)
-      if (rChildNdx < this.size && this.cost(this._heap[rChildNdx]) > this.cost(this._heap[lChildNdx])) {
-        lChildNdx = rChildNdx
-      }
-      if (this.cost(this._heap[ndx]) < this.cost(this._heap[lChildNdx])) {
-        this.swap(ndx, lChildNdx)
-        ndx = lChildNdx
-      } else {
-        break
-      }
-    }
-  }
-}
+const cache = {}
 
 export const solve = (data) => {
   // Parse towels
   const towels = new Set(data[0].split(', '))
-  const maxTowelLength = _.maxBy(data[0].split(', '), towel => towel.length).length
-  // Count valid patterns
-  console.log(data.slice(2).filter(pattern => {
-    // Create list of checked indeces
-    const checked = Array(pattern.length).fill(false)
-    // Create max heap of indexes to try and add 0
-    const heap = new MaxHeap()
-    heap.push(0)
-    // While you still have indexes to try
-    while (heap.size > 0) {
-      // Take the largest index and mark it as checked
-      const patternNdx = heap.pop()
-      checked[patternNdx] = true
-      // Look for matching towels from size 1 to max towel length
-      for (let length = 1; length <= maxTowelLength; length++) {
-        // If this has already been checked, skip it
-        if (checked[patternNdx + length]) continue
-        // If you find a matching towel, add the new index to the heap
-        const substring = pattern.substring(patternNdx, patternNdx + length)
-        if (towels.has(substring)) {
-          // If you reached the end of the pattern, quit
-          if (patternNdx + length === pattern.length) return true
-          heap.push(patternNdx + length)
-        }
+  // Check each pattern
+  console.log(data.slice(2).filter(pattern => isPossible(towels, pattern)).length)
+}
+
+const isPossible = (towels, pattern) => {
+  // Check the cache
+  if (cache[pattern] !== undefined) return cache[pattern]
+  // Check if this pattern is a whole towel
+  if (towels.has(pattern)) return true
+  // Try every single point split of the pattern
+  for (let i = 1; i < pattern.length; i++) {
+    // If the left side is a towel, recurse with the right side
+    if (towels.has(pattern.slice(0, i))) {
+      if(isPossible(towels, pattern.slice(i))) {
+        // Update the cache and return when you find something
+        cache[pattern] = true
+        return true
       }
     }
-    // No matching pattern found
-    return false
-  }).length)
+  }
+  // Update the cache and return that you didn't find something
+  cache[pattern] = false
+  return false
 }
--- 2024/day19/part02.js
+++ 2024/day19/part02.js
@@ -1,5 +1,27 @@
 import _ from 'lodash'
 
+const cache = {}
+
 export const solve = (data) => {
-  console.log('This problem has not been attempted yet.')
+  // Parse towels
+  const towels = new Set(data[0].split(', '))
+  // Check each pattern
+  console.log(_.sum(data.slice(2).map(pattern => matchCount(towels, pattern))))
+}
+
+const matchCount = (towels, pattern) => {
+  // Check the cache
+  if (cache[pattern] !== undefined) return cache[pattern]
+  // Check if this pattern is a whole towel
+  let count = towels.has(pattern) ? 1 : 0
+  // Try every single point split of the pattern
+  for (let i = 1; i < pattern.length; i++) {
+    // If the left side is a towel, recurse with the right side
+    if (towels.has(pattern.slice(0, i))) {
+      count += matchCount(towels, pattern.slice(i))
+    }
+  }
+  // Update the cache and return the count
+  cache[pattern] = count
+  return count
 }
